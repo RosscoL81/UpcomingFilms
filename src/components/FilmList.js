db83775872adb7fa9b4d970f899502885c5d73aa
Ross Lorimer <wross.lorimer@googlemail.com>
2019-12-09 20:23:55 +0000

    homework completed

---
+++ src/components/FilmList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import Film from './Film'
+
+class FilmList extends Component{
+
+  render(){
+    const filmNodes = this.props.films.map(film => {
+      return (
+        <Film
+          name={film.name}
+          key={film.id}
+          url={film.url}
+          >
+        </Film>
+      )
+    })
+
+    return (
+      <ul className="FilmList">
+        {filmNodes}
+      </ul>
+    )
+  }
+}
+
+export default FilmList
---
+++ src/containers/UpcomingFilms.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import FilmList from '../components/FilmList'
+
+class UpcomingFilms extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      films: [
+        {
+          id: 1,
+          name: "Spider-Man: Into the Spider-Verse",
+          url: "https://www.imdb.com/title/tt4633694/?ref_=rlm"
+        },
+        {
+          id: 2,
+          name: "Life Itself",
+          url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+        },
+        {
+          id: 3,
+          name: "Mary Queen of Scots",
+          url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+        },
+        {
+          id: 4,
+          name: "The Lego Movie 2: The Second Part",
+          url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+        },
+        {
+          id: 5,
+          name: "Captain Marvel",
+          url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="UpcomingFilms">
+        <h1>Upcoming Films in the UK</h1>
+        <FilmList films={this.state.films}/>
+        <form action="https://www.imdb.com/calendar/?region=gb">
+          <input type="submit" value ="View More Upcoming Releases" />
+        </form>
+      </div>
+
+
+    )
+  }
+
+}
+
+export default UpcomingFilms
